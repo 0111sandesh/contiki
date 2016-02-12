3a23fd5f8300f461ebf4621af3326fcefd7435cf
Sandesh <0111sandesh@gmail.com>
2016-02-12 02:11:07 -0500

    added my hello example

---
+++ examples/hello/testing/test1/prog.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "prog.h"
+
+void printer(){
+	printf("This is a new program.\n");
+}
---
+++ examples/hello/testing/test1/prog2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "prog2.h"
+
+void printer(){
+	printf("This is a new program.\n");
+}
